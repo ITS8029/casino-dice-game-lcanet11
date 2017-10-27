f84e0f0ca522bf9ce90eabc3b911f9b8c30610d1
lcanet11 <Luci.Canet@gmail.com>
2017-10-26 21:05:46 -0400

    Update Die.java

--- com/example/Die.java
+++ com/example/Die.java
@@ -1,13 +1,43 @@
-package com.example;
+import com.example.Die;
 
-import java.util.Random;
+public class DiceGame {
 
-public class Die {
-	public int numberOfSides = 6;
-	
-	public int roll() {
-		Random random = new Random();
+	public static void main(String[] args) {
 		
-		return random.nextInt(6) + 1;
+		int rollTotal=0;
+		int finalTotal=0;
+		int player1Total=0;
+		int player1final=0;
+		
+		while(rollTotal!=7) {
+			// TODO Auto-generated method stub
+			Die die1=new Die(); 
+			Die die2=new Die(); 
+			rollTotal=die1.roll()+die2.roll();
+			System.out.println("The total is: " + rollTotal);
+			finalTotal+=rollTotal;
+		}
+		
+		System.out.println(finalTotal);
+		
+		while(player1Total!=7) {
+			// TODO Auto-generated method stub
+			Die die1=new Die(); 
+			Die die2=new Die(); 
+			player1Total=die1.roll()+die2.roll();
+			System.out.println("The total is: " + player1Total);
+			player1final+=player1Total;
+		}
+		
+	
+	System.out.println(player1final);
+	
+	
+	if (player1final>finalTotal){
+		System.out.println("The player wins: "+player1final);
+	} else if (player1final<finalTotal) {
+		System.out.println("The player loses: -"+player1final);
+	} else System.out.println("It's a draw: " + 0);
 	}
+	
 }
