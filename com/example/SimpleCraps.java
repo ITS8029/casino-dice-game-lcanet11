c42daff26331948a8c700dac9fa3a450be68a535
lcanet11 <Luci.Canet@gmail.com>
2017-10-30 11:54:07 -0400

    Update SimpleCraps.java

--- com/example/SimpleCraps.java
+++ com/example/SimpleCraps.java
@@ -4,7 +4,44 @@ public class SimpleCraps implements BettingGame {
 
 	@Override
 	public int playGame(int betAmount) {
-		// TODO Add your code here. Return the amount the user won (or a negative number if they lost!)
+		
+		int rollTotal=0;
+		int finalTotal=0;
+		int player1Total=0;
+		int player1final=0;
+		
+		while(rollTotal!=7) {
+			// TODO Auto-generated method stub
+			Die die1=new Die(); 
+			Die die2=new Die(); 
+			rollTotal=die1.roll()+die2.roll();
+			System.out.println("The total is: " + rollTotal);
+			finalTotal+=rollTotal;
+		}
+		
+		System.out.println(finalTotal);
+		
+		while(player1Total!=7) {
+			// TODO Auto-generated method stub
+			Die die1=new Die(); 
+			Die die2=new Die(); 
+			player1Total=die1.roll()+die2.roll();
+			System.out.println("The total is: " + player1Total);
+			player1final+=player1Total;
+		}
+		
+	
+	System.out.println(player1final);
+	
+	
+	if (player1final>finalTotal){
+		System.out.println("The player wins: "+player1final);
+	} else if (player1final<finalTotal) {
+		System.out.println("The player loses: -"+player1final);
+	} else System.out.println("It's a draw: " + 0);
+	}
+	
+	// TODO Add your code here. Return the amount the user won (or a negative number if they lost!)
 		
 		//To create a die, use:
 		//Die die = new Die();
