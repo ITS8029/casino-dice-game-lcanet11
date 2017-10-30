0733107ec215af717793c44eca203aff8a1c91eb
lcanet11 <Luci.Canet@gmail.com>
2017-10-30 13:52:49 -0400

    Update Die.java

--- com/example/Die.java
+++ com/example/Die.java
@@ -1,1 +1,28 @@
 
+package com.example;
+
+
+
+import java.util.Random;
+
+
+
+public class Die {
+
+	public int numberOfSides = 6;
+
+	
+
+	public int roll() {
+
+		//Return a random number between 1 and numberOfSides
+
+		Random myRandom = new Random();
+
+		
+
+		return myRandom.nextInt(numberOfSides) + 1; 
+
+	}
+
+}
